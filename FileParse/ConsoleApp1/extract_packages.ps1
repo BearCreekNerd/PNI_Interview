8dc8c27af15d2c0700e7ed1c787a87c7d876bc14
Parvinder Grewal <parvinderg@outlook.com>
2021-04-29 21:47:03 -0700

    Filter for Unique Objects

--- FileParse/ConsoleApp1/extract_packages.ps1
+++ FileParse/ConsoleApp1/extract_packages.ps1
@@ -1,8 +1,9 @@
 $files = Get-ChildItem -Recurse -Filter *csproj 
 
 foreach ($file in $files){
-    $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include}
+    $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique
     #$csproj_content
     #$csproj_content.Project.ItemGroup.ProjectReference | ForEach-Object {$_.Node.value}
    # $csproj_content | Select-Xml -XPath /Project/ItemGroup/ProjectReference | Select-Object -ExpandProperty Node
+   
 }
