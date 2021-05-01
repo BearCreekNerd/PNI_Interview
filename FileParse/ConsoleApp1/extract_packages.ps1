a508a1015dfcd17de265de2771bdf896c8a2d704
Parvinder Grewal <parvinderg@outlook.com>
2021-04-30 21:33:33 -0700

    Output package info to a logfile and append

--- FileParse/ConsoleApp1/extract_packages.ps1
+++ FileParse/ConsoleApp1/extract_packages.ps1
@@ -1,9 +1,11 @@
 $files = Get-ChildItem -Recurse -Filter *csproj 
-
+#$logfile = Out-File -FilePath .\log.txt
 foreach ($file in $files){
-    $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique
+    $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique | Out-File -FilePath .\log.txt -Append
     #$csproj_content
     #$csproj_content.Project.ItemGroup.ProjectReference | ForEach-Object {$_.Node.value}
    # $csproj_content | Select-Xml -XPath /Project/ItemGroup/ProjectReference | Select-Object -ExpandProperty Node
+
+   #$file | Out-File -FilePath .\log.txt -Append
    
-}
+} 
