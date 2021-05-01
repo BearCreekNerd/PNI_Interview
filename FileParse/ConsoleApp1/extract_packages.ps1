fa6a46a6ebfbe7b7e48c782670c85ff4f8d708bf
Parvinder Grewal <parvinderg@outlook.com>
2021-05-01 16:36:06 -0700

    some clean and further experimentation

--- FileParse/ConsoleApp1/extract_packages.ps1
+++ FileParse/ConsoleApp1/extract_packages.ps1
@@ -1,11 +1,11 @@
 $files = Get-ChildItem -Recurse -Filter *csproj 
 #$logfile = Out-File -FilePath .\log.txt
 foreach ($file in $files){
-    $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique | Out-File -FilePath .\log.txt -Append
-    #$csproj_content
-    #$csproj_content.Project.ItemGroup.ProjectReference | ForEach-Object {$_.Node.value}
-   # $csproj_content | Select-Xml -XPath /Project/ItemGroup/ProjectReference | Select-Object -ExpandProperty Node
+    #$file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique | Out-File -FilePath .\log.txt -Append
+    Write-Host $file
+    $software_list = $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | Select-Object -ExpandProperty Node 
+    $software_list
+}
 
-   #$file | Out-File -FilePath .\log.txt -Append
-   
-} 
+#TODO: Figure out how to take the extracted data and insert into a dotnet package statement
+#dotnet add package $package_list.Include --version $package_list.Version  --package-directory C:\test_dir
