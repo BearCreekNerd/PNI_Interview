a19380e6d548c529a4c8ad5805898979e7153556
Parvinder Grewal <parvinderg@outlook.com>
2021-05-01 19:03:15 -0700

    playing with a hash based approach

--- FileParse/ConsoleApp1/extract_packages.ps1
+++ FileParse/ConsoleApp1/extract_packages.ps1
@@ -1,27 +1,36 @@
 $files = Get-ChildItem -Recurse -Filter *csproj 
-#$xpath = /Project/ItemGroup/PackageReference
-#$logfile = Out-File -FilePath .\log.txt
+$solution_file = Get-ChildItem -Recurse -Filter *sln
+
+$solution_file
+
 $result = @( foreach ($file in $files){
     #$file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique | Out-File -FilePath .\log.txt -Append
-    #Write-Host $file
     $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | Select-Object -ExpandProperty Node
-
-    #| Select-Object ExpandProperty Node
-    
     }
 )
-#$result | ForEach-Object {$_.Version}
-
-$software_name = $result | ForEach-Object {$_.Include}
-
-$software_version = $result | ForEach-Object {$_.Version}
 
 
-$software_name
-$software_version
+$software_name = $result | ForEach-Object {$_.Include} | Select-Object -Unique
+$software_version = $result | ForEach-Object {$_.Version} | Select-Object -Unique
 
 
 
+
+$result_list = @{
+    name = $software_name
+    version = $software_version
+}
+
+foreach ($h in $result_list.Values) 
+{
+    #write-host $h.name
+    #write-host $h.version
+   dotnet add package  $h.name.Item --version $h.version --package-directory c:\test_dir2
+}
+
+
+#$software_name
+#$software_version
 
 
 #TODO: Figure out how to take the extracted data and insert into a dotnet package statement
