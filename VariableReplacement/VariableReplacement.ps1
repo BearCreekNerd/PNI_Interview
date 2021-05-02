132af0d088bc0c9b46f268e8f490f98a92e9ebff
Parvinder Grewal <parvinderg@outlook.com>
2021-05-01 22:02:31 -0700

    test iterating through the json object

--- VariableReplacement/VariableReplacement.ps1
+++ VariableReplacement/VariableReplacement.ps1
@@ -1,3 +1,21 @@
 $json_file = Get-Content .\variables.json | ConvertFrom-Json
 
-$json_file
+#$json_file.variables.value
+
+$name_array = @($json_file.variables.Name)
+$value_array = @($json_file.variables.value)
+
+
+foreach ($items in $name_array){
+    $items.ToString()
+}
+
+foreach ($items in $value_array){
+    $items.ToString()
+
+}
+
+#TODO
+#load the xml file
+#set set the xml attributes from the extraced values of the JSON file.
+#create a new hashtable with the names as the keys and the values as the values
