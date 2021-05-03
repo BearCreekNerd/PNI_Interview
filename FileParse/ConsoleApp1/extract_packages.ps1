c62b9bf8df49471728a5a045f9919cd7a30ef25d
Parvinder Grewal <parvinderg@outlook.com>
2021-05-02 19:05:07 -0700

    Able to use finally make this work with a hashtable. Using the Install-Module command worked better in this scenario.Also some cleanup.

--- FileParse/ConsoleApp1/extract_packages.ps1
+++ FileParse/ConsoleApp1/extract_packages.ps1
@@ -1,59 +1,27 @@
 $files = Get-ChildItem -Recurse -Filter *csproj 
+$packages_hash = @{}
 
-#Get-Content $files
+#$software_name = $files | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object { $_.Node.Include }
+#$software_version = $files | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object { $_.Node.Version }
 
-$software_name = $files | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object { $_.Node.Include }
-$software_version = $files | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object { $_.Node.Version }
-
-
-$software_name
-$software_version
-
-$result = @( foreach ($file in $files){
-    #$file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | ForEach-Object {$_.Node.Include, $_.Node.version} | Select-Object -Unique | Out-File -FilePath .\log.txt -Append
-        $file | Select-Xml -Xpath /Project/ItemGroup/PackageReference | Select-Object -ExpandProperty Node
+foreach($file in $files){
+    $xml_nodes = Select-Xml -Xpath /Project/ItemGroup/PackageReference -Path $file
+    foreach($xml in $xml_nodes){
+        Write-Host $xml.Node.Include
+        Write-Host $xml.Node.Version
+        $packages_hash.Add($xml.Node.Include, $xml.Node.Version)
     }
-) 
-
-$result
-
-
-
-$logfile = $result | Out-File .\log.txt
-$logfile
-
-Write-Host "---------"
-Write-Host "---------"
-
-
-$read_log = Get-Content .\log.txt
-
-Write-Host $read_log
-
-foreach($lines in $read_log){
-     Write-Host $lines
+    
 }
 
-# #Write result to a logfile.
+$packages_hash
+#Set-Location  .\ClassLibrary1
+foreach($key in $packages_hash.Keys){
+    Install-Package $key -RequiredVersion $packages_hash[$key] -Destination C:\temp_dir1 -SkipDependencies
+}
+
+Write-Host "Thank you and have a nice day!"
+
+exit 
 
 
-
-# foreach($key in $result.keys)
-# {
-#     $message = '{0} is {1} years old' -f $key, $result[$key]
-#     Write-Output $message
-# }
-
-# $result_list = @{
-#    name = $software_name
-#    version = $software_version
-# }
-
-# Write-Host $result_list[$software_name]
-
-# foreach ($items in $result_list){
-#     Write-Host $items
-# }
-
-#TODO: Figure out how to take the extracted data and insert into a dotnet package statement
-#dotnet add package $package_list.Include --version $package_list.Version  --package-directory C:\test_dir
--- FileParse/ConsoleApp1/extract_packages2.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Recurse -Filter *csproj  
-
-
-
-$result2 = $files | Select-Xml -Xpath /Project/ItemGroup/PackageReference | Select-Object {$_.Node.Include} | Group-Object -AsHashTable
-
-foreach ($item in $result2.Values){
-    $item
-}
