2988fa4f4cfec070563a7c767a54eebee6921fe3
Parvinder Grewal <parvinderg@outlook.com>
2021-05-02 21:53:44 -0700

    clean up and map the values correctly

--- VariableReplacement/VariableReplacement.ps1
+++ VariableReplacement/VariableReplacement.ps1
@@ -1,32 +1,33 @@
 #Get-Content .\variables.json -Raw | Test-Json
 $json_file = Get-Content -Raw .\variables.json | ConvertFrom-Json 
 $json_objects_hash = @{}
-#$xml_file = Get-Content .\App.config | Select-Xml -XPath /configuration/appSettings/add | ForEach-Object {$_.Node.add.keys}
+
+#$json_file.variables
+#$json_file.variables[2]
 
 
-$json_file.variables[2]
-
-
-foreach ($json_objects in $json_file.variables){
-    Write-Host $json_objects
-    #$json_objects_hash.Add($json_file.keys)
-
+foreach ($json_object in $json_file.variables){
+    $json_objects_hash.Add($json_object.Name, $json_object.Value)
 }
 
-Write-Host $json_objects_hash
-#Write-Host $xml_file
+$json_objects_hash
 
+# This code does not work --> it does not overwrite the attribute in the file
+$xmlDoc = [XML](Get-Content ".\App_Test.config")
+ foreach ($item in  $xmlDoc.configuration.appSettings.add){
+   # $item.value = "Perry is dumb."
+   # $item.key = "Perry is smartish"
 
-#This code does not work --> it does not overwrite the attribute in the file
-<# $xmlDoc = [XML](Get-Content ".\App_Test.config")
-foreach ($item in  $xmlDoc.configuration.appSettings.add){
-    {$item.value = "Perry is dumb."}
-    {$item.key = "Perry is smart"}
+    if ($item.value -match "#{(.+)}"){
+        $item.value = $json_objects_hash[$matches[1]]
+    }
 }
-$xmlDoc.Save("App_Test.config") #>
+$xmlDoc.Save("App_Test.config")
 
 #TODO
 #load the xml file
 #set set the xml attributes from the extraced values of the JSON file.
 #create a new hashtable with the names as the keys and the values as the values
 #overwrite the value attributes in the xml file with the correct value from i.e."Value": "PNI"
+
+
