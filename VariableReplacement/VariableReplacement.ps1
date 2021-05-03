eb80878b52084f698194a5c1e71a0151c64595e8
Parvinder Grewal <parvinderg@outlook.com>
2021-05-02 20:57:23 -0700

    figuring out these json / xml madness again

--- VariableReplacement/VariableReplacement.ps1
+++ VariableReplacement/VariableReplacement.ps1
@@ -17,6 +17,7 @@ Write-Host $json_objects_hash
 #Write-Host $xml_file
 
 
+#This code does not work --> it does not overwrite the attribute in the file
 <# $xmlDoc = [XML](Get-Content ".\App_Test.config")
 foreach ($item in  $xmlDoc.configuration.appSettings.add){
     {$item.value = "Perry is dumb."}
@@ -28,3 +29,4 @@ $xmlDoc.Save("App_Test.config") #>
 #load the xml file
 #set set the xml attributes from the extraced values of the JSON file.
 #create a new hashtable with the names as the keys and the values as the values
+#overwrite the value attributes in the xml file with the correct value from i.e."Value": "PNI"
